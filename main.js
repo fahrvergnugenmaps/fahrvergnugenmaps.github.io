9f68ff37cf363d5d7dad62ea519e334b77f58713
fahrvergnugenmaps <tomas.nils322@gmail.com>
2024-06-25 23:10:44 -0700

    testing points

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXNubjk4IiwiYSI6ImNseHI4MHg4ZzBrdXQyam9tbmR2Y2ZlZHQifQ.naW1BbxBbf20EERFLqREng';
 
-// Initialize the map
-var map = new mapboxgl.Map({
-  container: 'map', // container id specified in the HTML
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-121.98685827727454,37.360250540945366], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-121.98685827727454, 37.360250540945366],
+    zoom: 9
 });
+
+// Fetch points from the JSON file and add them to the map
+fetch('points.json')
+    .then(response => response.json())
+    .then(geojson => {
+        geojson.features.forEach(function(marker) {
+            // Create a HTML element for each feature
+            var el = document.createElement('div');
+            el.className = 'marker';
+
+            // Make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+                .addTo(map);
+        });
+    })
+    .catch(error => console.error('Error fetching GeoJSON:', error));
+   
