d71ca6fa45493e427d1f8e862b1d58c979da80bf
fahrvergnugenmaps <tomas.nils322@gmail.com>
2024-06-26 22:12:48 -0700

    coded json into js file

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXNubjk4IiwiYSI6ImNseHI4MHg4ZzBrdXQyam9tbmR2Y2ZlZHQifQ.naW1BbxBbf20EERFLqREng';
 
+const geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.99086461286373, 37.408798455507124]
+            },
+            "properties": {
+                "title": "Apple",
+                "description": "Job number one"
+            }
+        }
+    ]
+};
+
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -8,22 +25,18 @@ const map = new mapboxgl.Map({
     zoom: 9
 });
 
-// Fetch points from the JSON file and add them to the map
-fetch('points.json')
-    .then(response => response.json())
-    .then(geojson => {
-        geojson.features.forEach(function(marker) {
-            // Create a HTML element for each feature
-            var el = document.createElement('div');
-            el.className = 'marker';
+console.log('Adding markers from GeoJSON data...');
+geojson.features.forEach(function(marker) {
+    console.log('Adding marker:', marker);
 
-            // Make a marker for each feature and add to the map
-            new mapboxgl.Marker(el)
-                .setLngLat(marker.geometry.coordinates)
-                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-                .addTo(map);
-        });
-    })
-    .catch(error => console.error('Error fetching GeoJSON:', error));
-   
+    // Create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+
+    // Make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+        .addTo(map);
+});
