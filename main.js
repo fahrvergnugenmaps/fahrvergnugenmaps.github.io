af81e7704dd36956d74688846604ee9de1d008b5
fahrvergnugenmaps <tomas.nils322@gmail.com>
2024-06-29 23:39:54 -0700

    added pony ai point

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Replace 'YOUR_MAPBOX_ACCESS_TOKEN' with your Mapbox access token
-mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXNubjk4IiwiYSI6ImNseHI4MHg4ZzBrdXQyam9tbmR2Y2ZlZHQifQ.naW1BbxBbf20EERFLqREng';
+console.log('foo');
 
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXNubjk4IiwiYSI6ImNseHI4MHg4ZzBrdXQyam9tbmR2Y2ZlZHQifQ.naW1BbxBbf20EERFLqREng';
 const geojson = {
     "type": "FeatureCollection",
     "features": [
@@ -11,8 +12,21 @@ const geojson = {
                 "coordinates": [-121.99086461286373, 37.408798455507124]
             },
             "properties": {
+            	"type": "Apple",
                 "title": "Apple",
                 "description": "Job number one"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ -121.94056278133901, 37.47480348449061]
+            },
+            "properties": {
+            	"type": "Pony",
+                "title": "Pony.ai",
+                "description": "Job number two"
             }
         }
     ]
@@ -22,7 +36,7 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-121.98685827727454, 37.360250540945366],
-    zoom: 9
+    zoom: 8
 });
 
 console.log('Adding markers from GeoJSON data...');
@@ -32,6 +46,14 @@ geojson.features.forEach(function(marker) {
     // Create a HTML element for each feature
     var el = document.createElement('div');
     el.className = 'marker';
+	console.log('made it')
+
+    if (marker.properties.type === "Apple") {
+    	console.log('found apple')
+        el.classList.add('apple');
+    } else if (marker.properties.type === "Pony") {
+        el.classList.add('pony');
+    }
 
     // Make a marker for each feature and add to the map
     new mapboxgl.Marker(el)
