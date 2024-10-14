d9c19df33acffbe65e7c6cb9194b458600e83ddd
fahrvergnugenmaps <tomas.nils322@gmail.com>
2024-10-13 23:44:09 -0700

    added more stuff

--- kmltoshp/kmltoshp.py
+++ kmltoshp/kmltoshp.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import sys
 from datetime import datetime
 
-kml_file = "sys.argv[0]"
+kml_file = sys.argv[0]
 gdf = gpd.read_file(kml_file)
 
 shapefile_path = "/Users/tom/Documents/GitHub/fahrvergnugenmaps.github.io/kmltoshp/shapefile.shp"
