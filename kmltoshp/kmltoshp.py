fb04acbde4a0efd3a1779c163f0f28678f40f23c
fahrvergnugenmaps <tomas.nils322@gmail.com>
2024-10-25 23:19:08 -0700

    break from schemacon, starting danmap

--- kmltoshp/kmltoshp.py
+++ kmltoshp/kmltoshp.py
@@ -1,9 +1,9 @@
 import geopandas as gpd
-import sys
-from datetime import datetime
+#import sys
+#from datetime import datetime
 
-kml_file = sys.argv[0]
-gdf = gpd.read_file(kml_file)
+#kml_file = sys.argv[0]
+#gdf = gpd.read_file(kml_file)
 
-shapefile_path = "/Users/tom/Documents/GitHub/fahrvergnugenmaps.github.io/kmltoshp/shapefile.shp"
-gdf.to_file(shapefile_path, driver="ESRI Shapefile")
+#shapefile_path = "/Users/tom/Documents/GitHub/fahrvergnugenmaps.github.io/kmltoshp/shapefile.shp"
+#gdf.to_file(shapefile_path, driver="ESRI Shapefile")
